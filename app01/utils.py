4ff0e64b18f38089d7c052cb2da3aed9f18ce456
13242084001 <13242084001@163.com>
2019-06-02 16:41:49 +0100

    更新代码

--- app01/utils.py
+++ app01/utils.py
@@ -35,36 +35,36 @@ class CmdHandle(object):
 
     def __init__(self, controlling_ip, **kwargs):
         self.controlling_ip = controlling_ip
-        for i in kwargs:
-            for k in i:
-                self.__dict__[k] = i.get(k)
-        self.make_iptables_list()
-        self.make_tc_list()
+        self.rule = kwargs.get('rule')
+        self.upstream = kwargs.get('upstream')
+        self.downstream = kwargs.get('downstream')
+        self.iptables_cmd_list, self.local_ip, self.mark = self.make_iptables_list()
+        self.tc_cmd_list, self.parent_id = self.make_tc_list()
 
     def make_iptables_list(self):
-        
-        protocol = "all" if not self.protocol else self.protocol
 
-        local_ip = self.controlling_ip if not self.local_ip else self.local_ip
+        protocol = "All" if not self.rule.get("protocol") else self.rule.get("protocol")
+
+        local_ip = self.controlling_ip if not self.rule.get("local_ip") else self.rule.get("local_ip")
 
         src_string = " -s %s" % local_ip
-        
-        if not self.local_port:
+
+        if not self.rule.get("local_port"):
             sport_string = ""
-        elif len(str(self.local_port).split(",")) > 1:
-            sport_string = " –m multiport --sports %s" % self.local_port
+        elif len(str(self.rule.get("local_port")).split(",")) > 1:
+            sport_string = " –m multiport --sports %s" % self.rule.get("local_port")
         else:
-            sport_string = " --dport %s" % local_port
-        
-        dst_string = "" if not remote_ip else " -d %s" % self.remote_ip
-        
-        if not self.remote_port:
+            sport_string = " --dport %s" % self.rule.get("local_port")
+
+        dst_string = "" if not self.rule.get("remote_ip") else " -d %s" % self.rule.get("remote_ip")
+
+        if not self.rule.get("remote_ip"):
             dport_string = ""
 
-        elif len(str(self.remote_port).split(",")) > 1:
-            dport_string = " –m multiport --dports %s" % self.remote_port
+        elif len(str(self.rule.get("remote_ip")).split(",")) > 1:
+            dport_string = " –m multiport --dports %s" % self.rule.get("remote_ip")
         else:
-            dport_string = " --dport %s" % self.remote_port
+            dport_string = " --dport %s" % self.rule.get("remote_ip")
         iptables_cmd_list = []
         try:
             last_mark = models.Info.objects.all().last().mark
@@ -75,7 +75,7 @@ class CmdHandle(object):
         print(mark)
         mark_string = " -j MARK --set-mark %d" % mark
         if dst_string or dport_string:
-            if protocol == "all":
+            if protocol == "All":
                 if not dport_string:
                     cmd = "iptables -t mangle -A PREROUTING" + dst_string + " -p %s" % protocol + dport_string + mark_string
                     iptables_cmd_list.append(cmd)
@@ -88,7 +88,7 @@ class CmdHandle(object):
                 cmd = "iptables -t mangle -A PREROUTING" + dst_string + " -p %s" % protocol + dport_string + mark_string
                 iptables_cmd_list.append(cmd)
 
-        if protocol == "all":
+        if protocol == "All":
             if not sport_string:
                 cmd = "iptables -t mangle -A PREROUTING" + src_string + " -p %s" % protocol + dport_string + mark_string
                 iptables_cmd_list.append(cmd)
@@ -103,78 +103,84 @@ class CmdHandle(object):
 
         for cmd in iptables_cmd_list:
             print(cmd)
+        return iptables_cmd_list, local_ip, mark
 
-        self.iptables_cmd_list = iptables_cmd_list
-        self.local_ip = local_ip
-
-    def confirm_dev(self):
+    def confirm_dev(self, updown):
         inet_obj = GetIfaces()
         net_dict = inet_obj.interfaces_dict
         print(net_dict)
         for k, v in net_dict.items():
-            print(type(v.get("hosts")))
+            # print(type(v.get("hosts")))
             if self.controlling_ip in v.get("hosts"):
-                net_dict.pop(k)
-                return net_dict.keys()
+                if updown == "upstream":
+                    net_dict.pop(k)
+                    print(net_dict.keys())
+                    return net_dict.keys()
+                elif updown == "downstream":
+
+                    return k,
 
     def make_tc_list(self):
-        dev = self.confirm_dev()
-
-        if self.rate:
-            rate_string = " rate %skbps" % self.rate
-        else:
-            rate_string = " rate 10mbit"
-
-        if self.loss.get("percentage"):
-            if self.loss.get("correlation"):
-                loss_string = " loss %s%% %s%%" % (self.loss.get("percentage"), self.loss.get("correlation"))
-            else:
-                loss_string = " loss %s%%" % self.loss.get("percentage")
-        else:
-            loss_string = ""
-
-        if self.delay.get("delay"):
-            delay_string = " delay %s%%" % self.delay.get("delay")
-        else:
-            delay_string = ""
-
-        if self.corruption.get("percentage"):
-            corruption_string = " corruption %s%%" % self.corruption.get("percentage")
-        else:
-            corruption_string = ""
-
-        if self.reorder.get("percentage"):
-
-            if self.reorder.get("correlation"):
-                reorder_string = " reorder %s%% %s%%" % (self.reorder.get("percentage"), self.reorder.get("correlation"))
-            else:
-                reorder_string = " reorder %s%%" % self.reorder.get("percentage")
-        else:
-            reorder_string = ""
-
-        try:
-            parent_id = models.Info.objects.all().last().parent_id
-            parent_id += 1
-        except Exception:
-            parent_id = 22
-        class_id = int(str(parent_id)[-1] + str(parent_id))
         tc_cmd_list = []
-        for nic in dev:
-            cmd1 = "tc class add dev %s parent 1: classid 1:%s htb rate 10mbps" % (nic, parent_id)
-            cmd2 = "tc class add dev %s parent 1:%s class 1:%s htb" % (nic, parent_id, class_id) + rate_string
-            cmd3 = "tc qdisc add dev %s parent 1:%s netem" % (nic, parent_id) + loss_string + delay_string + \
-                   corruption_string + reorder_string
-            tc_cmd_list.extend([cmd1, cmd2, cmd3])
-        self.tc_cmd_list = tc_cmd_list
-        self.parent_id = parent_id
+        for i in ["upstream", "downstream"]:
+            dev = self.confirm_dev(i)
+            updown = getattr(self, i)
+            if updown.get("rate"):
+                rate_string = " rate %skbps" % updown.get("rate")
+            else:
+                rate_string = " rate 10mbit"
 
-    def exec_cmd():
+            if updown.get("loss").get("percentage"):
+                if updown.get("loss").get("correlation"):
+                    loss_string = " loss %s%% %s%%" % (updown.get("loss").get("percentage"), updown.get("loss").get("correlation"))
+                else:
+                    loss_string = " loss %s%%" % updown.get("loss").get("percentage")
+            else:
+                loss_string = ""
+
+            if updown.get("delay").get("delay"):
+                delay_string = " delay %sms" % updown.get("delay").get("delay")
+            else:
+                delay_string = ""
+
+            if updown.get("corruption").get("percentage"):
+                corruption_string = " corruption %s%%" % updown.get("corruption").get("percentage")
+            else:
+                corruption_string = ""
+
+            if updown.get("reorder").get("percentage"):
+
+                if updown.get("reorder").get("correlation"):
+                    reorder_string = " reorder %s%% %s%%" % (updown.get("reorder").get("percentage"), updown.get("reorder").get("correlation"))
+                else:
+                    reorder_string = " reorder %s%%" % updown.get("reorder").get("percentage")
+            else:
+                reorder_string = ""
+
+            try:
+                parent_id = models.Info.objects.all().last().parent_id
+                parent_id += 1
+            except Exception:
+                parent_id = 22
+            class_id = int(str(parent_id)[-1] + str(parent_id))
+
+            for nic in dev:
+                cmd1 = "tc class add dev %s parent 1: classid 1:%s htb rate 10mbps" % (nic, parent_id)
+                cmd2 = "tc class add dev %s parent 1:%s classid 1:%s htb" % (nic, parent_id, class_id) + rate_string
+                cmd3 = "tc qdisc add dev %s parent 1:%s netem" % (nic, class_id) + loss_string + delay_string + \
+                       corruption_string + reorder_string
+                cmd4 = "tc filter add dev %s protocol ip handle %s fw classid 1:%s" % (nic, self.mark, class_id)
+                tc_cmd_list.extend([cmd1, cmd2, cmd3, cmd4])
+        print(tc_cmd_list)
+        return tc_cmd_list, parent_id
+
+    def exec_cmd(self):
         for cmd_list in (self.iptables_cmd_list, self.tc_cmd_list):
-            for cmd in cmd_list
-                pass
-            # ouput = subprocess.getstatusoutput(rule)
-            # if ouput[0]:
-            #    return ouput[1]
-    
+            for cmd in cmd_list:
+                ouput = subprocess.getstatusoutput(cmd)
+                print(ouput[0])
+                if ouput[0]:
+                    print(ouput[1])
+
     def __call__(self):
         self.exec_cmd()
--- app01/views.py
+++ app01/views.py
@@ -14,17 +14,20 @@ def init():
     nic_list.remove("lo")
     for nic in nic_list:
         print(nic)
-        out = subprocess.getstatusoutput("tc qdisc add dev %s root htb" % (nic,))
+        subprocess.getstatusoutput("tc qdisc delete dev %s root" % (nic,))
+        out = subprocess.getstatusoutput("tc qdisc add dev %s root handle 1: htb default 11" % (nic,))
         if out[0]:
             print(out[1])
 
 init()
 
+
 class ControlingInfo(APIView):
     def get(self, request):
-        controling_ip = request.META.get("REMOTE_ADDR")
-        print(controling_ip)
-        return Response({"client_ip": controling_ip})
+        controlling_ip = request.META.get("REMOTE_ADDR")
+        print(controlling_ip)
+        controlled_ip_list = models.Info.objects.filter(controlling_ip=controlling_ip).all().values("controlled_ip", "status")
+        return Response({"client_ip": controlling_ip, "controlled_ip_list": controlled_ip_list})
 
 
 class AddControlledIp(APIView):
@@ -63,46 +66,101 @@ class Profile(APIView):
 
 
 class Shaping(APIView):
-    def post(self, request):
+    def get(self, request, **kwargs):
+        controlling_ip = request.META.get("REMOTE_ADDR")
+        ip = kwargs.get("slug")
+        print(ip)
+        obj = models.Info.objects.filter(controlling_ip=controlling_ip, controlled_ip=ip, status=1).first()
+        print(obj.profile_pk.content)
+        print("#################")
+        print(type(obj.profile_pk.content))
+        return Response(eval(obj.profile_pk.content))
+
+    def post(self, request, **kwargs):
         print(request.data)
         controlling_ip = request.META.get("REMOTE_ADDR")
-
         handle = CmdHandle(controlling_ip, **request.data)
         iptables_cmd_list = handle.iptables_cmd_list
         local_ip = handle.local_ip
         tc_cmd_list = handle.tc_cmd_list
         parent_id = handle.parent_id
+        mark = handle.mark
 
         try:
             # 如果该被控制ip有一个规则，则判断该规则是不是有名字的，如果是，则新增规则，并修改外键pk；如果没有名字，则修改该规则
             profile_obj = models.Info.objects.filter(controlled_ip=local_ip).first().profile_pk
+            print(profile_obj)
+            print("************")
+            print(111111111111)
             if not profile_obj.name:
+                print(profile_obj.name)
                 #   没有名字
                 models.Profile.objects.filter(id=profile_obj.id).update(content=request.data)
+                print(2222222222)
                 #models.Info.objects.filter(controlled_ip=local_ip).update(profile_pk=new_profile_obj)
             else:
                 #   有名字
                 new_profile_obj = models.Profile.objects.create(content=request.data)
                 models.Info.objects.filter(controlled_ip=local_ip).update(profile_pk=new_profile_obj)
+
         except Exception:
             # 如果报错了，说明还没有这条shaping记录
             profile_obj = models.Profile.objects.create(content=request.data)
+            print(33333333333)
             models.Info.objects.create(controlling_ip=controlling_ip, controlled_ip=local_ip,
                                        iptables=iptables_cmd_list, parent_id=parent_id, tc=tc_cmd_list,
-                                       profile_pk=profile_obj)
+                                       profile_pk=profile_obj, status=1, mark=mark)
+            print(4444444444)
 
         # 执行相关命令
         handle()
         return Response(request.data)
 
+        #   删除某个正在shaping的ip相关规则，也就是停止
+    def delete(self, request, **kwargs):
+        ip = kwargs.get("slug")
+        print(ip)
+        obj = models.Info.objects.filter(controlled_ip=ip).first()
+        if obj:
+            profile_obj = obj.profile_pk  # 返回profile对象
+            print(profile_obj, type(profile_obj))
+            # print(obj.iptables, type(obj.iptables))
+            # 将iptables命令替换成删除命令
+            iptables_list = list(map(lambda x: x.replace("-A", "-D"), eval(obj.iptables)))  # eval方法将列表字符串转换成列表
+            print(iptables_list)
+            # tc 删除命令
+            parent_id = obj.parent_id
+            dev_list = netifaces.interfaces()
+            dev_list.remove("lo")
+            print(dev_list)
+            #tc_list = list(map(lambda x: "tc qdisc delete dev %s parent 1:%s" % (x, parent_id), dev_list))
+            #print(tc_list)
+            # tc删除不了，要想办法呀
+            for i in iptables_list:
+                subprocess.getstatusoutput(i)
 
+            obj.update(status=0)
+            if not profile_obj.name:
+                profile_obj.delete()
+
+        return Response(None)
+
+    def put(self, request, **kwargs):
+        self.delete(request, **kwargs)
+        self.post(request, **kwargs)
+        return Response(None)
+
+"""
 class Action(APIView):
     def get(self, request, **kwargs):
-        obj = models.Info.objects.all().values()
-        print(json.dumps(list(obj)))
-        obj1 = models.Profile.objects.all().values()
-        print(json.dumps(list(obj1)))
-        return Response(None)
+        controlling_ip = request.META.get("REMOTE_ADDR")
+        ip = kwargs.get("slug")
+        print(ip)
+        obj = models.Info.objects.filter(controlling_ip=controlling_ip, controlled_ip=ip).first()
+        print(obj.profile_pk.content)
+        print("#################")
+        print(type(obj.profile_pk.content))
+        return Response(eval(obj.profile_pk.content))
 
     def post(self, request):
         return Response(None)
@@ -121,15 +179,18 @@ class Action(APIView):
             print(iptables_list)
             #tc 删除命令
             parent_id = obj.parent_id
-            tc_list = list(map(lambda x: "tc qdisc delete dev %s parent 1:%s" % (x, parent_id), netifaces.interfaces().remove('lo')))
-            if IptablesHandle.exec_iptables(*iptables_list):
-                #   如果命令没有执行成功
-                pass
-            # 执行tc命令
-            TcHandle.exec_tc(*tc_list)
+            dev_list = netifaces.interfaces()
+            dev_list.remove("lo")
+            print(dev_list)
+            tc_list = list(map(lambda x: "tc qdisc delete dev %s parent 1:%s" % (x, parent_id), dev_list))
+            print(tc_list)
+            for i in (iptables_list, tc_list):
+                for k in i:
+                    subprocess.getstatusoutput(k)
+
             obj.delete()
             if not profile_obj.name:
                 profile_obj.delete()
 
         return Response(None)
-
+"""
